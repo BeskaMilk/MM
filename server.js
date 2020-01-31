0d8edde5c21b6f2906ceced6f551c1c061ae7447
Thoare <kadyhumn@gmail.com>
2020-01-31 12:24:11 +0800

    dotenv included

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-if(process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-} 
+// if(process.env.NODE_ENV !== 'production') {
+//     require('dotenv').config()
+// } 
+
+require('dotenv').config()
+
 
 const express = require('express')
 const app = express()
